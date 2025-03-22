e7f36fa1de6b0c249b76b55b017954a446807ebf
Aman Singh <amankrs212@gmail.com>
2025-03-23 01:01:50 +0530

    Refactor secret loading and API key handling; streamline memory management and improve code readability

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -2,12 +2,22 @@ import os
 import json
 
 
-# Load secrets.json
-def load_secrets(file_path="src/secrets.json"):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"{file_path} not found!")
+SECRET_PATH = "src/secrets.json"
 
-    with open(file_path, "r") as file:
+
+# Load secrets.json
+def load_secrets():
+    if not os.path.exists(SECRET_PATH):
+        with open(SECRET_PATH, "w") as file:
+            json.dump(
+                {
+                    "MISTRAL_API_KEY": "YOUR_API_KEY"
+                },
+                file,
+                indent=4,
+            )
+
+    with open(SECRET_PATH, "r") as file:
         secrets = json.load(file)
 
     return secrets
